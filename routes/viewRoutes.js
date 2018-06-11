eeb36566720f170d9f6164cf509d0ef63ec00a0a
SkyNet <nme312.noreply.github.com>
2018-06-11 14:55:50 -0500

    main handelbars template

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,16 @@
 module.exports = function(app) {
-    app.get("/", function(req, res){
+    app.get("/home", function(req, res){
         var obj = {};
-        res.render("index", obj);
+        res.render("home", obj);
+    });
+
+    app.get("/new", function(req, res){
+        var obj = {};
+        res.render("newArticles", obj);
+    });
+
+    app.get("/saved", function(req, res){
+        var obj = {};
+        res.render("savedArticles", obj);
     });
 }
